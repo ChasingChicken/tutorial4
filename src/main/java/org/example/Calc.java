389759f1175ea42eae5fa815ee9bfebc724b3f35
Reuben <reubenalsop44@gmail.com>
2024-08-14 12:51:10 +1200

    Added subtraction method

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Calc {
 
+    public void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2));
+    }
+
     public int add(int x, int y)
     {
         return x+y;
